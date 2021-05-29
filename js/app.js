4e8fe9121a602d3c40615172c0e14df559b06fb4
Hamza <muhammadhamzam1486@gmail.com>
2021-05-29 22:21:34 +0500

    ADD preloader

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
 AOS.init();
+
+const containerDots = document.querySelector('.dots-container');
+
+const removePreloader = function () {
+  containerDots.style.display = 'none';
+};
+
+window.addEventListener('load', removePreloader);
